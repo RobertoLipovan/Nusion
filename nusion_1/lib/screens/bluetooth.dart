5b17cf5cad5df8e63945f2c535120458ea8b0ac8
lipov <lowbuster23@gmail.com>
2024-02-10 15:50:16 +0100

    Tercer commit

--- nusion_1/lib/screens/bluetooth.dart
+++ nusion_1/lib/screens/bluetooth.dart
@@ -20,6 +20,9 @@ class _BluetoothDevicesState extends State<BluetoothDevices> {
   void startScan() async {
     PermissionStatus locationStatus = await Permission.location.status;
     PermissionStatus bluetoothStatus = await Permission.bluetooth.status;
+    PermissionStatus bluetoothScanStatus = await Permission.bluetoothScan.status;
+    PermissionStatus bluetoothAdvertiseStatus = await Permission.bluetoothAdvertise.status;
+    PermissionStatus bluetoothConnectStatus = await Permission.bluetoothConnect.status;
 
     if (!locationStatus.isGranted) {
       PermissionStatus result = await Permission.location.request();
@@ -30,6 +33,17 @@ class _BluetoothDevicesState extends State<BluetoothDevices> {
       }
     }
 
+    if (!bluetoothScanStatus.isGranted) {
+      PermissionStatus result = await Permission.bluetoothScan.request();
+      if (!result.isGranted) {
+        // El usuario no concedió el permiso de Bluetooth
+        print('El usuario no concedió el permiso de Bluetooth Scan');
+        return;
+      }
+    }
+
+    
+
     if (!bluetoothStatus.isGranted) {
       PermissionStatus result = await Permission.bluetooth.request();
       if (!result.isGranted) {
@@ -38,6 +52,28 @@ class _BluetoothDevicesState extends State<BluetoothDevices> {
         return;
       }
     }
+
+    /*
+
+    if (!bluetoothAdvertiseStatus.isGranted) {
+      PermissionStatus result = await Permission.bluetoothAdvertise.request();
+      if (!result.isGranted) {
+        // El usuario no concedió el permiso de Bluetooth
+        print('El usuario no concedió el permiso de Bluetooth Advertise');
+        return;
+      }
+    }
+
+    if (!bluetoothConnectStatus.isGranted) {
+      PermissionStatus result = await Permission.bluetoothConnect.request();
+      if (!result.isGranted) {
+        // El usuario no concedió el permiso de Bluetooth
+        print('El usuario no concedió el permiso de Bluetooth Connect');
+        return;
+      }
+    }
+
+    */
 
     try {
       await for (var device in flutterReactiveBle.scanForDevices(
